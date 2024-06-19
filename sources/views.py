99efc839c2112c1ad043771a0b5f0e3cb3eef095
Carlos Barreto <contato.cerb@outlook.com>
2024-06-18 22:42:23 -0300

    fix: root and pages

--- sources/views.py
+++ sources/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'pages/index.html')
+
+def integrante(request):
+    return render(request, 'pages/integrante.html')
+
+def administrador(request):
+    return render(request, 'pages/administrador.html')
